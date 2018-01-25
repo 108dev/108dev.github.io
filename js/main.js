157739f28840bcf4dd149cb51f116dbb9b1d0641
niksher <nikita_sherstnev@mail.ru>
2018-01-25 15:28:45 +0300

    date in footer and title on index

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-$(document).ready(function() {
-    var date = new Date();
-    var y = date.getFullYear();
-    var m = date.getMonth() + 1;
-    var d = date.getDate();
-    $(".footer__date").html(d + "." + m + "." + y); 
-});
+
 
 
